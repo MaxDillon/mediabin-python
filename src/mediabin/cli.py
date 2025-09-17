1b5c98205245c3cd1deb7d9e2cd0ae91b53ff9b7
Max Dillon <maxdillon13124@gmail.com>
2025-09-16 22:28:44 -0700

    refactor: combine restart logic with start and stop

--- src/mediabin/cli.py
+++ src/mediabin/cli.py
@@ -8,7 +8,7 @@ from mediabin.daemon import Daemon
 from mediabin.ytdlp_downloader import YTDLPDownloader, DownloadOptions, StatusDownloading, StatusFinished, StatusError, StatusPending
 
 class MyDaemon(Daemon):
-    def on_spawn(self, ):
+    def on_spawn(self):
         self.active_downloads: Dict[str, YTDLPDownloader] = {}
         self.download_queue: queue.Queue = queue.Queue()
         self.completed_downloads: Dict[str, YTDLPDownloader] = {}
@@ -64,38 +64,27 @@ def main(
     stop_service: bool = typer.Option(False, "--stop-service", help="Stops the mediabin daemon service."),
     restart_service: bool = typer.Option(False, "--restart-service", help="Restarts the mediabin daemon service (stops then starts)."),
 ):
+    if ctx.invoked_subcommand is not None:
+        return
     service_options = [start_service, stop_service, restart_service]
     if sum(service_options) > 1:
         raise typer.BadParameter("Cannot specify more than one of --start-service, --stop-service, or --restart-service.")
     elif sum(service_options) == 0:
-        if ctx.invoked_subcommand is None:
-            raise typer.BadParameter("Missing option: One of --start-service, --stop-service, or --restart-service")
+        cli = typer.main.get_command(app)
+        typer.echo(cli.get_help(ctx))  # reuse the current ctx here
 
-    if start_service:
-        if daemon.is_process_running():
-            print(f"Daemon is already running")
-            raise typer.Exit(code=1)
-        print("Starting mediabin daemon service...")
-        pid = daemon.spawn()
-        print(f"Started with pid: {pid}")
-    elif stop_service:
+    if stop_service or restart_service:
         print("Stopping mediabin daemon service...")
         try:
             daemon.stop()
         except ProcessLookupError:
             print("no service to stop")
-    elif restart_service:
-        print("Restarting mediabin daemon service...")
-        try:
-            daemon.stop()
-            print("Daemon stopped.")
-        except ProcessLookupError:
-            print("No existing daemon to stop, proceeding with start.")
-        
-        if daemon.is_process_running(): # Check if it's still running for some reason
-            print(f"Daemon is still running after stop attempt. Cannot restart.")
+    
+    if start_service or restart_service:
+        if daemon.is_process_running():
+            print(f"Daemon is already running")
             raise typer.Exit(code=1)
-
+        print("Starting mediabin daemon service...")
         pid = daemon.spawn()
         print(f"Started with pid: {pid}")
 
@@ -103,9 +92,6 @@ def main(
 @app.command("ping")
 @daemon.command
 def ping_command():
-    for i in range(4):
-        print(i)
-        time.sleep(1)
     print("pong")
 
 @app.command("install")
